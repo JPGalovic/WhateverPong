5bfe2c8a2fecc2d045d32b7c4d3c471a9fd0d3a5
James P. Galovic <jpgalovic@gmail.com>
2017-12-19 00:19:25 +1100

    Change: Removed Barriers, Difficulty increases

--- Assets/Script/MovePaddle.cs
+++ Assets/Script/MovePaddle.cs
@@ -7,13 +7,29 @@ public class MovePaddle : MonoBehaviour {
     public UnityEvent onUp;
     public UnityEvent onDown;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float
+        clampMarginMinX = 0.0f,
+        clampMarginMaxX = 0.0f,
+        clampMarginMinY = 0.0f,
+        clampMarginMaxY = 0.0f;
+
+    private float
+        clampMinX,
+        clampMaxX,
+        clampMinY,
+        clampMaxY;
+
+    // Use this for initialization
+    void Start()
+    {
+        clampMinX = Camera.main.ScreenToWorldPoint(new Vector2(0 + clampMarginMinX, 0)).x;
+        clampMaxX = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width - clampMarginMaxX, 0)).x;
+        clampMinY = Camera.main.ScreenToWorldPoint(new Vector2(0, 0 + clampMarginMinY)).y;
+        clampMaxY = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height + clampMarginMaxY)).y;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey("up"))
             onUp.Invoke();
 
@@ -23,6 +39,9 @@ public class MovePaddle : MonoBehaviour {
 
     public void Move(float n)
     {
-        this.gameObject.transform.position += new Vector3(0, n);
+        Vector3 position = transform.position;
+        transform.position += new Vector3(0, n);
+        if (transform.position.y <= clampMinY || transform.position.y >= clampMaxY)
+            transform.position = position; //step back
     }
 }
